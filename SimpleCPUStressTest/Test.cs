188bb2460d551f0748c82c70417a31aa5b559c71
Jurij15 <jurij.grom1@gmail.com>
2022-08-14 17:24:52 +0200

    Add project files.

---
+++ SimpleCPUStressTest/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCPUStressTest
+{
+    public static class Test
+    {
+        public static void StopTest()
+        {
+
+        }
+        public static bool PrepTest(bool bAutostart)
+        {
+
+            return false;
+        }
+        public static void StartTest()
+        {
+
+        }
+    }
+}
